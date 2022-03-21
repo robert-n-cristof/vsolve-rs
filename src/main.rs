fa7e5064d680b2425750a1f005bbad604d1cfb19
Robert Cristof <robert.n.cristof@gmail.com>
2022-03-21 07:58:57 -0500

    Add flags for brute force key search for vsolve

--- src/main.rs
+++ src/main.rs
@@ -8,32 +8,49 @@ use getopts::Options;
 use std::env;
 
 // use getopts::Options;
-const USAGE: &str = "vsolve key input-string";
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options] [key] input-string", program);
+    print!("{}", opts.usage(&brief));
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // let _program = &args[0];
+    let program = args[0].clone();
 
     let mut opts = Options::new();
     opts.optflag("d", "decode", "decode input");
+    opts.optflagopt("b", "brute", "bruteforce key [optional len]", "LEN");
+
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
         Err(f) => {
-            panic!("{}", f.to_string())
+            println!("{}", f.to_string());
+            print_usage(&program, opts);
+            return;
         }
     };
 
     if matches.free.len() < 2 {
-        println!("Usage: {}", USAGE);
-        std::process::exit(1);
+        print_usage(&program, opts);
+        return;
     }
 
     let key = &matches.free[0];
     let input = matches.free[1..].join(" ");
 
     if input.is_empty() || key.is_empty() {
-        println!("Usage: {}", USAGE);
-        std::process::exit(1);
+        print_usage(&program, opts);
+        return;
+    }
+
+    if matches.opt_present("b") {
+        let brute_len = matches.opt_get_default::<i32>("b", -7).unwrap();
+        let best = vigenere::brute_force(brute_len, &input);
+        let plaintext = vigenere::decode(best.as_str(), &input);
+        let chi = stats::chi_squared(&plaintext, &english::FREQUENCY);
+
+        println!("{} [{}] {}", chi, best, plaintext);
+        return;
     }
 
     let result = if matches.opt_present("d") {
@@ -43,4 +60,6 @@ fn main() {
     };
 
     println!("{}", result);
+    let chi = stats::chi_squared(result.as_str(), &english::FREQUENCY);
+    println!("chi: {}", chi);
 }
